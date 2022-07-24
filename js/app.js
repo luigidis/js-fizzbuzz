5133047743b3a91aed959d47e1b14b757f5c02c2
LuigiDiS <luigi.distefano69@gmail.com>
2022-07-24 13:39:09 +0200

    Bonus 1 + Bonus 2

--- js/app.js
+++ js/app.js
@@ -1,25 +1,45 @@
 // console.log('ciao')
-const gridElement = document.getElementsByClassName('grid')
-console.log(gridElement)
-gridElement.innerHTML = ''
-
+//Recuper l'elemnto nel DOM dove metterò le mie celle
+const gridElement = document.querySelector('.grid')
+// console.log(gridElement)
 for (let i = 1; i <= 100; i++) {
     //Stampo i numeri da 1 a 100
     // console.log(i)
+    //Creare un div che avrà la classe cell per creare la "scacchiera"
+    const divElement = document.createElement('div')
     // Se è un multiplo di 5 e di 3 dovrò scrivere FizzBuzz
     if (((i % 3) === 0) && ((i % 5) === 0 )) {
-        console.log('FizzBuzz')
+        // console.log('FizzBuzz')
+        divElement.classList.add('cell_fizzbuzz')
+        divElement.append('FizzBuzz')
     } else if ((i % 3) === 0) {
         // Se è un multiplo di 3 dovrò scrivere Fizz al posto del numero
-        console.log('Fizz')
+        // console.log('Fizz')
+        divElement.classList.add('cell_fizz')
+        divElement.append('Fizz')
     } else  if ((i % 5) === 0 ) {
         // Se è un multiplo di 5 dovro scrivere Buzz
-        console.log ('Buzz')
+        // console.log ('Buzz')
+        divElement.classList.add('cell_buzz')
+        divElement.append('Buzz')
     } else {
         // Sennò è un numero qualsiasi
-        console.log (i)
+        // console.log (i)
+        divElement.classList.add('cell_blue')
+        divElement.append(i)
     }
+    divElement.classList.add('cell')
+    // console.log(divElement)
+    gridElement.append(divElement)
 }
+    
+    
+
+
+
+
+
+
 
     
     
